377fe4928bba6e0a184e7d4c34146d503ee11e45
Neino00 <honoreneino@gmail.com>
2025-08-08 21:59:12 +0100

    Update index.js
    Index view

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client, logger } = require('./lib/client')
-const { DATABASE, VERSION } = require('./config')
+const { DATABASE, VERSION, AUTO_STATUS_VIEW } = require('./config') // ⬅ Import de AUTO_STATUS_VIEW
 const { stopInstance } = require('./lib/pm2')
 
 const start = async () => {
@@ -14,6 +14,20 @@ const start = async () => {
   try {
     const bot = new Client()
     await bot.connect()
+
+    // 🔹 Activation de la vue automatique des statuts
+    if (AUTO_STATUS_VIEW) {
+      logger.info("AUTO_STATUS_VIEW activé : lecture automatique des statuts.")
+      bot.on('status-update', async (update) => {
+        try {
+          await bot.readStatus(update.id)
+          logger.info(`Statut vu : ${update.id}`)
+        } catch (err) {
+          logger.error(`Erreur lecture statut : ${err.message}`)
+        }
+      })
+    }
+
   } catch (error) {
     logger.error(error)
   }
